c80a01a03dbb7b16af78437f16c4ef2b0df84205
Marina Moshnogorskaya <marinamoshnogorskaya@gmail.com>
2018-06-19 13:54:25 +0300

    toggler works

--- src/search/search.js
+++ src/search/search.js
@@ -7,6 +7,12 @@ import Results from './results';
 import ToggleView from './toggle-view';
 
 class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isList: true,
+    }
+  }
   render() {
     return (
        <div className='search'>
--- src/search/toggle-view.js
+++ src/search/toggle-view.js
@@ -1,19 +1,45 @@
 import React, { Component } from "react";
-import FontAwesomeIcon from '@fortawesome/react-fontawesome';
-import faThLarge from '@fortawesome/fontawesome-free-solid/faThLarge';
-import faBars from '@fortawesome/fontawesome-free-solid/faBars';
-import './toggle-view.css';
-
+import FontAwesomeIcon from "@fortawesome/react-fontawesome";
+import faThLarge from "@fortawesome/fontawesome-free-solid/faThLarge";
+import faBars from "@fortawesome/fontawesome-free-solid/faBars";
+import "./toggle-view.css";
 
 class ToggleView extends Component {
+  constructor(props) {
+    super(props);
+    this.handleDashboardClick = this.handleDashboardClick.bind(this);
+    this.handleListClick = this.handleListClick.bind(this);
+    this.state = {
+      isList: true
+    };
+  }
+
+  handleDashboardClick() {
+    this.setState({
+      isList: false
+    });
+  }
+
+  handleListClick() {
+    this.setState({
+      isList: true
+    });
+  }
   render() {
     return (
-        <div className='view'>
-        <a className='view__dashboard'>
-        <FontAwesomeIcon icon={faThLarge} />
+      <div className="view">
+        <a
+          className={
+            !this.state.isList ? "view__dashboard active" : "view__dashboard"
+          }
+        >
+          <FontAwesomeIcon
+            icon={faThLarge}
+            onClick={this.handleDashboardClick}
+          />
         </a>
-        <a className='view__list'>
-        <FontAwesomeIcon icon={faBars} />
+        <a className={this.state.isList ? "view__list active" : "view__list"}>
+          <FontAwesomeIcon icon={faBars} onClick={this.handleListClick} />
         </a>
       </div>
     );
