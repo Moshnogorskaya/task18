571083471ac681ca590f58b4ea88c11f472fe26a
Marina Moshnogorskaya <marinamoshnogorskaya@gmail.com>
2018-06-19 13:34:32 +0300

    toggle placeholder ready

--- src/search/search-panel/input-keywords.js
+++ src/search/search-panel/input-keywords.js
@@ -2,11 +2,49 @@ import React, { Component } from "react";
 import "./input-keywords.css";
 
 class InputKeywords extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      active: false,
+      value: ""
+    };
+  }
+
+  handleClick() {
+    this.setState({
+      active: true
+    });
+  }
+
+  handleInput(e) {
+    this.setState({
+      value: e.target.value
+    });
+  }
+
+  handleBlur() {
+    if (!this.state.value) {
+      this.setState({
+        active: false
+      });
+    }
+  }
   render() {
     return (
       <div className="input">
-        <p className="input__placeholder">Type here for search</p>
+        <p
+          className={
+            this.state.active
+              ? "input__placeholder active"
+              : "input__placeholder"
+          }
+        >
+          Type here for search
+        </p>
         <input
+          onFocus={this.handleClick.bind(this)}
+          onInput={this.handleInput.bind(this)}
+          onBlur={this.handleBlur.bind(this)}
           type="text"
           maxLength="50"
           className="input__field"
@@ -17,3 +55,4 @@ class InputKeywords extends Component {
 }
 
 export default InputKeywords;
+("input__placeholder");
